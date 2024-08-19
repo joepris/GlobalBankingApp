16b5c7dd32fa40d7f2905ee9f239e3e8db51d349
JoePris <calpito.jp@gmail.com>
2024-08-19 09:00:27 -0600

    Add project files.

---
+++ GlobalBankingApp/Controllers/LoginController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GlobalBankingApp.Controllers
+{
+    public class LoginController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(string username)
+        {
+            if (!string.IsNullOrEmpty(username))
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            ViewBag.ErrorMessage = "Please enter a username.";
+            return View();
+        }
+    }
+}
+
